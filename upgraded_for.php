538c9568a2719fd8c0523e4014019ecc2948df60
danieljunek15 <dvandenbrink@roc-dev.com>
2019-10-18 20:17:45 +0200

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,19 @@
+<?php
+$name = array(
+    "joe",
+    "mama",
+    "pizza",
+    "gabber joost",
+    "minecraft is goed",
+    "fortnite niet",
+);
+
+$nummer = array(
+1,2,3,4,5,6,7,8,9,10
+);
+
+foreach ($name as $value) {
+    echo "$value" .PHP_EOL;
+}
+
+exit();
